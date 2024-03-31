a34c1a7e8c42963751d6430f3846e48d8b42bd84
Asadullah <asadullahasad942@gmail.com>
2024-03-31 06:44:25 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,10 @@ export default function Home() {
       <li className="font-bold text-white"><Link href={'/About-us'}>About-us</Link></li>
       <li className="font-bold text-white"><Link href={'/Home'}>Home</Link></li>
       </ul></nav>
-      <div className="bg-blue-500 h-96"><div className="text-center py-40 "><p className="mx-16 text-3xl ">Contact with us please click the button below</p>
-      <button type="button" onClick={()=>router.push('/Contact-us')} className=" font-bold bg-red-700 px-4 py-2 mx-16 rounded-full hover:bg-slate-200 border-black border">Contact-us</button></div></div>
+      <div className="bg-blue-500 h-96"><div className="text-center py-40 "><p className="mx-16 text-3xl ">Contact with us please click on the button below</p>
+      <button type="button" onClick={()=>router.push('/Contact-us')} className=" font-bold bg-red-700 px-4 py-2 mx-16 rounded-full hover:bg-slate-200 border-black border">Contact-us</button>
+      <p className="text-3xl pt-12">Click on the page names on the nav bar to move from one page to Another</p>
+      </div></div>
 
       </body>
 
